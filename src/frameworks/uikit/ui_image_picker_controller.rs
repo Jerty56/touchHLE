72ec7dcad9c44f8361cd33d7c707dfa0679dfa0b
hikari_no_yume <hikari@noyu.me>
2023-03-25 19:37:27 +0100

    hack: return nil from image picker init (FIX ME)

--- src/frameworks/uikit/ui_image_picker_controller.rs
+++ src/frameworks/uikit/ui_image_picker_controller.rs
@@ -6,7 +6,7 @@
 //! `UIImagePickerController`
 
 use crate::frameworks::foundation::NSInteger;
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{id, nil, objc_classes, release, ClassExports};
 
 type UIImagePickerControllerSourceType = NSInteger;
 
@@ -16,12 +16,17 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 // TODO: should extend UINavigationController, which extends
 //       UIViewController.
-@implementation UIImagePickerController
+@implementation UIImagePickerController: NSObject
 
 + (bool)isSourceTypeAvailable:(UIImagePickerControllerSourceType)_type {
     // For now, simply claim no sources are available.
     // TODO: support some sources.
     false
+}
+
+- (id)init {
+    release(env, this);
+    nil // FIXME: real implementation
 }
 
 @end
